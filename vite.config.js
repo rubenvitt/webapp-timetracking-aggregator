a38affef1138a3643d9a687ec5b3c8b3dab64f04
Ruben Vitt <ruben.vitt@innoq.com>
2022-07-20 16:29:11 +0200

    🐳(cmd): fix start command

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import("vite").UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		port: 5000,
+		host: true
+	}
 };
 
 export default config;
